686e186a608f1bf10892df7b71bce96e05e3afc8
Ben-C <sneblot@gmail.com>
2019-04-11 15:13:13 +0100

    Added user number choice

--- countNumbers.cpp
+++ countNumbers.cpp
@@ -4,9 +4,14 @@
 
 
 int countNumbers() {
+	int userNum;
+	std::cout << "Please enter in a number: " << std::endl;
+	std::cin >> userNum;
+
 	int i = 0;
-	while(i < 100){
+	while(i < userNum){
 		std::cout << i << std::endl; i++;
 	}
+
 	return 0;	
 }
