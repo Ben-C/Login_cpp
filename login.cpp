efae382a413ac1a4c40b1ea090b940e8509b74df
Ben-C <sneblot@gmail.com>
2019-05-21 15:39:59 +0100

    Cleaned up code added user ID No.

--- login.cpp
+++ login.cpp
@@ -12,17 +12,18 @@ int def(){
 
 int regUser(){
 	bool cont;
+	int userID = 0;
 	while(cont = true){
-	std::string userName, userPass;
-	std::cout << "select a username: \n"; std::cin >> userName;
-	std::cout << "select a password: \n"; std::cin >> userPass;
-	std::ofstream uLog;
-	uLog.open("username.csv", std::ios::app | std::ios::out);
-	uLog << userName << "," << userPass << std::endl;
-	uLog.close();
-	std::cout << "Would you like to Enter another user? Y/N" << std::endl;
-	std::string userChoice;
-	std::cin >> userChoice;
+		std::string userName, userPass;
+		std::cout << "select a username: \n"; std::cin >> userName;
+		std::cout << "select a password: \n"; std::cin >> userPass;
+		std::ofstream uLog;
+		uLog.open("username.csv", std::ios::app | std::ios::out);
+		uLog << userID++ << "," << userName << "," << userPass << std::endl;
+		uLog.close();
+		std::cout << "Would you like to Enter another user? Y/N" << std::endl;
+		std::string userChoice;
+		std::cin >> userChoice;
 	if(userChoice == "Y"){
 		cont = true;
 	} 
@@ -36,34 +37,32 @@ void checkLogin(){
 	std::string userName, userPass;
 	std::cout << "Enter your username: \n"; std::cin >> userName;
 	std::cout << "Enter your password: \n"; std::cin >> userPass;
-	
+	std::string uID, uN, uP;
 	std::ifstream userLog("username.csv", std::ios::in);
 	if (userLog.is_open()){
-		while (!userLog.eof()){
-			std::string uN, uP;
-			userLog >> uN >> uP;
-			if (userName == uN && userPass == uP){
+		while (userLog >> uN >> uP){
+			getline(userLog, uN);
+			getline(userLog, uP);
+			if (uN == userName && uP == userPass){
 				std::cout << "Login Succseful!" << std::endl;
 				countNumbers();
 				break;
 			}
-			else
-			{
+			else{
 				std::cout << "Login failed, programe will terminate!" << std::endl;
 				break;
 			}
 		}
 	}
-		else 
-		{
+		else{
 			userLog.close();
 		}
 }
 
-int login() {
+int login(){
 	int choice;
 	std::cout << "1: Register\n2: Login\n"; std::cin >> choice;
-	switch (choice)	{
+	switch (choice){
 		default:
 			def();
 		break;
